3789f6e943579ff0a2732d44c12e90473d18b3b5
ShemetViktoriia <shemetvic@ukr.net>
2019-11-12 01:05:50 +0200

    Layout was added #6

---
+++ MakeIt.WebUI/Controllers/CabinetController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System.Web.Mvc;
+
+namespace MakeIt.WebUI.Controllers
+{
+    public class CabinetController : BaseController
+    {
+        public CabinetController(IMapper mapper) : base(mapper)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }       
+    }
+}
