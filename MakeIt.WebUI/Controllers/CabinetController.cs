a44bbd4e354a9d80bed3acc6b032596b9bbe176d
miron50 <38781962+miron50@users.noreply.github.com>
2019-11-25 19:19:08 +0200

    In progress(EditTask,Dashboard)
    Add Create task(editTask)
    Add Dashboard

--- MakeIt.WebUI/Controllers/CabinetController.cs
+++ MakeIt.WebUI/Controllers/CabinetController.cs
@@ -1,18 +1,49 @@
 ﻿using AutoMapper;
 using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MakeIt.WebUI.ViewModel.Task;
+using System.Web.Mvc;
+using MakeIt.Repository.UnitOfWork;
+using System.Web.Security;
+using System.Security.Principal;
 
 namespace MakeIt.WebUI.Controllers
 {
     public class CabinetController : BaseController
     {
+        private string _user;
+        UnitOfWork unitOfWork;
+
         public CabinetController(IMapper mapper) : base(mapper)
         {
+            unitOfWork = new UnitOfWork();
         }
 
         [HttpGet]
         public ActionResult Index()
         {
-            return View();
-        }       
+            var userCurrentTasks = unitOfWork.Tasks.GetAll();
+           var userOldTasks = unitOfWork.Tasks.GetAll();
+
+            List<TaskShowViewModel> taskDisplay = new List<TaskShowViewModel>();/*
+            foreach(var test in userCurrentTasks)
+            {
+                TaskShowViewModel task = new TaskShowViewModel();
+                task.AssignedUser = test.AssignedUser.UserName.ToString();
+                task.Title = test.Title;
+                task.Priority = test.Priority.Name;
+                task.Project = test.Project.Name;
+                task.Milestone = test.Milestone.Title;
+                taskDisplay.Add(task);
+            }*/
+            ViewBag.CurrentTask = taskDisplay;
+            //ViewBag.OldTask = userOldTasks;
+            ViewBag.User = User.Identity.Name;
+            return View();           
+        }
+
     }
 }
---
+++ MakeIt.WebUI/ViewModel/Task/TaskShowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MakeIt.WebUI.ViewModel.Task
+{
+    public class TaskShowViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Milestone { get; set; }
+        public string Priority { get; set; }
+        public string Status { get; set; }
+        public string Project { get; set; }
+        public string AssignedUser { get; set; }
+    }
+}
