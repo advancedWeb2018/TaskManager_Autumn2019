73568420ab288984a0bd8ec11fc158ebeae9584f
ShemetViktoriia <shemetvic@ukr.net>
2019-11-26 22:37:03 +0200

    Fix ProjectViewModel validation

--- MakeIt.WebUI/ViewModel/Project/ProjectViewModel.cs
+++ MakeIt.WebUI/ViewModel/Project/ProjectViewModel.cs
@@ -8,9 +8,11 @@ namespace MakeIt.WebUI.ViewModel
         public int? Id { get; set; }
 
         [Required(ErrorMessage = "Name is required")]
+        [StringLength(20, ErrorMessage = "Maximum Length is 20 characters")]
         [Display(Name = "Name")]
         public string Name { get; set; }
 
+        [StringLength(500, ErrorMessage = "Maximum Length is 500 characters")]
         [Display(Name = "Description")]
         public string Description { get; set; }
 
