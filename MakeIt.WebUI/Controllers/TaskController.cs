325407768909587646775fdc8afc163b1e4b2f95
ShemetViktoriia <shemetvic@ukr.net>
2019-12-02 00:10:39 +0200

    Fix exception during creating and editing tasks

--- MakeIt.WebUI/Controllers/TaskController.cs
+++ MakeIt.WebUI/Controllers/TaskController.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using MakeIt.Repository.UnitOfWork;
-using MakeIt.WebUI.ViewModel;
+﻿using AutoMapper;
 using MakeIt.BLL.DTO;
 using MakeIt.BLL.Service.TaskOperations;
-using Microsoft.AspNet.Identity;
-using AutoMapper;
+using MakeIt.Repository.UnitOfWork;
 using MakeIt.WebUI.Filters;
+using MakeIt.WebUI.ViewModel;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace MakeIt.WebUI.Controllers
 {
@@ -17,10 +15,11 @@ namespace MakeIt.WebUI.Controllers
     public class TaskController : BaseController
     {
         private readonly ITaskService _taskService;
-        private UnitOfWork unitOfWork;
-        public TaskController(IMapper mapper, ITaskService  taskService) : base(mapper)
+        private IUnitOfWork _unitOfWork;
+        public TaskController(IMapper mapper, ITaskService  taskService, IUnitOfWork unitOfWork) : base(mapper)
         {
             _taskService = taskService;
+            _unitOfWork = unitOfWork;
         }
 
         [HttpGet]
@@ -35,31 +34,30 @@ namespace MakeIt.WebUI.Controllers
             {
                 return RedirectToAction("LogOff", "Account");
             }
-            unitOfWork = new UnitOfWork();
-            var currentUserobj = unitOfWork.Users.GetAll()
+            var currentUserobj = _unitOfWork.Users.GetAll()
                 .First(us => us.UserName.ToUpper().Equals(currentUser.ToUpper()));
 
-            var projects = unitOfWork.Projects.GetAll().ToList().Where(pr=>pr.Owner == currentUserobj || pr.Members.Contains(currentUserobj));
+            var projects = _unitOfWork.Projects.GetAll().ToList().Where(pr=>pr.Owner == currentUserobj || pr.Members.Contains(currentUserobj));
             var projNames = from proj in projects select proj.Name;
             SelectList project = new SelectList(projNames, "Project");
 
             ViewBag.Project = project;
 
            
-            var prior = unitOfWork.Priorities.GetAll();
+            var prior = _unitOfWork.Priorities.GetAll();
             var prName = from p in prior select p.Name;
             SelectList priority = new SelectList(prName, "Priority");
             ViewBag.Priority = priority;
 
-            var stone = unitOfWork.Milestones.GetAll();
+            var stone = _unitOfWork.Milestones.GetAll();
             var stoneNames = from s in stone select s.Title;
             SelectList milestones = new SelectList(stoneNames, "Milestone");
             ViewBag.Milestone = milestones;
 
-            var tu = unitOfWork.Users.GetAll().ToList();
+            var tu = _unitOfWork.Users.GetAll().ToList();
 
 
-            var users = unitOfWork.Users.GetAll();
+            var users = _unitOfWork.Users.GetAll();
             var userNames = from u in users select u.UserName;
             SelectList usersNames= new SelectList(userNames, "AssignedUser");
             ViewBag.Users = usersNames;
@@ -75,8 +73,7 @@ namespace MakeIt.WebUI.Controllers
             var userName = User.Identity.Name;
             int taskId = newTask.Id ?? default(int);
 
-            unitOfWork = new UnitOfWork();
-            var oldTask = unitOfWork.Tasks.Get(taskId);
+            var oldTask = _unitOfWork.Tasks.Get(taskId);
 
             var model = new TaskShowViewModel();
             if (newTask.Id == null)
@@ -122,8 +119,7 @@ namespace MakeIt.WebUI.Controllers
         [HttpGet]
         public ActionResult Show(int taskId)
         {
-            unitOfWork = new UnitOfWork();
-            var task = unitOfWork.Tasks.Get(taskId);
+            var task = _unitOfWork.Tasks.Get(taskId);
             TaskShowViewModel currentTask = new TaskShowViewModel();
 
             currentTask.Id = task.Id;
@@ -142,8 +138,7 @@ namespace MakeIt.WebUI.Controllers
         [HttpGet]
         public ActionResult Edit(int taskId)
         {
-            unitOfWork = new UnitOfWork();
-            var task = unitOfWork.Tasks.Get(taskId);
+            var task = _unitOfWork.Tasks.Get(taskId);
 
             TaskShowViewModel currentTask = new TaskShowViewModel();
             currentTask.Id = task.Id;
@@ -157,21 +152,21 @@ namespace MakeIt.WebUI.Controllers
             currentTask.CreatedUser = task.CreatedUser.UserName;
 
             string currentUser = User.Identity.Name;
-            var currentUserobj = unitOfWork.Users.GetAll()
+            var currentUserobj = _unitOfWork.Users.GetAll()
                 .First(us => us.UserName.ToUpper().Equals(currentUser.ToUpper()));
 
-            var projects = unitOfWork.Projects.GetAll().ToList().Where(pr => pr.Owner == currentUserobj || pr.Members.Contains(currentUserobj));
+            var projects = _unitOfWork.Projects.GetAll().ToList().Where(pr => pr.Owner == currentUserobj || pr.Members.Contains(currentUserobj));
             var projNames = from proj in projects select proj.Name;
             ViewBag.Project = new SelectList(projNames, "Project");
 
             
-            var priority = new SelectList(unitOfWork.Priorities.GetAll(), "Name", "Name");           
+            var priority = new SelectList(_unitOfWork.Priorities.GetAll(), "Name", "Name");           
             ViewBag.Priority = priority;
 
-            var statuses = new SelectList(unitOfWork.Statuses.GetAll(), "Name", "Name");
+            var statuses = new SelectList(_unitOfWork.Statuses.GetAll(), "Name", "Name");
             ViewBag.Status = statuses;
 
-            var usersForAssignee = new SelectList(unitOfWork.Users.GetAll(), "UserName", "UserName");
+            var usersForAssignee = new SelectList(_unitOfWork.Users.GetAll(), "UserName", "UserName");
             ViewBag.UsersForAssignee = usersForAssignee;
             
             return View(currentTask);
